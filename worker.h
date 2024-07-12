cc7423794427d7fa32f56c89a002b8ce693a87a6
via <122061686+dearivia@users.noreply.github.com>
2024-07-11 23:32:52 -0400

    Update worker.h

--- worker.h
+++ worker.h
@@ -27,11 +27,11 @@ public:
     void handle_message(Message msg);
 private:
     Network& network;
-    [[maybe_unused]] const CoordsMap& coords_to_thread;
-    [[maybe_unused]] const VecMap& y_idx_to_thread;
-    [[maybe_unused]] const ColSpMatrix& my_csc;
-    [[maybe_unused]] const SpVector& my_v;
-    [[maybe_unused]] SpVector& my_y;
+    const CoordsMap& coords_to_thread;
+    const VecMap& y_idx_to_thread;
+    const ColSpMatrix& my_csc;
+    const SpVector& my_v;
+    SpVector& my_y;
     const int id;
 
     // Can add more fields here
