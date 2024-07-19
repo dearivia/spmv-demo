2be118abd02ee0738b58c3a1073851ae79158e34
Olivia Tang <via@bcn1.csail.mit.edu>
2024-07-18 23:14:01 -0400

    skd

--- worker.cpp
+++ worker.cpp
@@ -75,6 +75,8 @@ void Worker::handle_message(Message msg) {
             y_updates_left[msg.coord]--;
             total_updates_left--;
             fmt::print("Updates left for coord {}: {}\n", msg.coord, y_updates_left[msg.coord]);
+            fmt::print("Total updates left: {}\n", total_updates_left);
+            fmt::print("Updates left for coord {}: {}\n", msg.coord, y_updates_left[msg.coord]);
             if (y_updates_left[msg.coord]==0){
                 fmt::print("Updated my_y[{}] to {}\n", msg.coord, my_y[msg.coord]);
                 my_y[msg.coord] = partial_sums[msg.coord];
